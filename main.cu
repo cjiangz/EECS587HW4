38fafb65644cbcd624efb5da0937a1fe2975f46d
Chris <jiangchristopher1@gmail.com>
2023-11-13 18:13:54 -0500

    Program works

--- main.cu
+++ main.cu
@@ -10,7 +10,8 @@
 #include <cstdlib>     /* atoi */
 #include <cmath>
 #include <vector>
-#include <thrust/device_vector.h>
+#include <iomanip>
+#include <limits>
 #include <numeric>
 
 
@@ -72,22 +73,34 @@ __global__ void DoIter(double* A_old, double* A_new)
     A_new[myGlobalId] = temp;
 }
 
-__global__ void reduce1(int *g_idata, int *g_odata) {
-    extern __shared__ int sdata[];
+__global__ void getVerificationValues(double* A, double* check1, double* check2){
+    int myGlobalId = getGlobalIdx_2D_2D();
+    int n_over_3_id = n/3 * gridDim.y * blockDim.y + n/3;
+    int nineteen_thirtyseven_id = 19 * gridDim.y * blockDim.y + 37;
+    if(myGlobalId == n_over_3_id){
+        *check1 = A[myGlobalId];
+    }
+    if(myGlobalId == nineteen_thirtyseven_id){
+        *check2 = A[myGlobalId];
+    }
+}
+
+__global__ void reduce(double* A_old, double* A_new, int N) {
+    __shared__ double sdata[1024];
 
     unsigned int tid = threadIdx.x;
     unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    sdata[tid] = g_idata[i];
+    sdata[tid] = i >= N ? 0 : A_old[i];
     __syncthreads();
 
-    for (unsigned int s=blockDim.x/2; s>0; s>>=1) {
+    for (unsigned int s=512; s>0; s>>=1) {
         if (tid < s) {
             sdata[tid] += sdata[tid + s];
         }
         __syncthreads();
     }
 
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+    if (tid == 0) A_new[blockIdx.x] = sdata[0];
 }
 
 
@@ -106,6 +119,9 @@ int main(int argc, char** argv)
     //vector<double> Az(rounded_n * rounded_n,0);
     double *A = new double[rounded_n*rounded_n];
     double *A_new = new double[rounded_n*rounded_n];
+    double sumCheck;
+    double check1;
+    double check2;
     for(int i = 0; i < rounded_n; ++i){
         for(int j=0;j<rounded_n;++j){
             if(i >= n_ || j >= n_){
@@ -125,9 +141,10 @@ int main(int argc, char** argv)
     //allocate memory for device variables
     double* d_Aold;
     double* d_Anew;
-    double* d_check1;
+    //int* d_n;
+    double *d_check1;
     double *d_check2;
-    double *d_sumCheck;
+
 
     //we can check if the cuda functions fail by seeing if they return a cudaSuccess code
     //you get status codes like cudaSuccess for free when you are compiling with nvcc
@@ -143,9 +160,11 @@ int main(int argc, char** argv)
     if(cudaMalloc(&d_check2,sizeof(double)) != cudaSuccess){
         cout<<"Could not allocate d_A"<<endl;
     }
-    if(cudaMalloc(&d_sumCheck,sizeof(double)) != cudaSuccess){
+    /*
+    if(cudaMalloc(&d_n,sizeof(int)) != cudaSuccess){
         cout<<"Could not allocate d_A"<<endl;
     }
+    */
 
     //copy values of A and B to d_a and d_b
 
@@ -164,7 +183,12 @@ int main(int argc, char** argv)
     cout << cudaGetErrorName(error) << endl;
      */
 
+    float elapsedTime = 0;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
+    cudaEventRecord(start);
     for(int i = 0; i < 10; ++i){
         swap(d_Aold, d_Anew);
         /*
@@ -174,7 +198,7 @@ int main(int argc, char** argv)
         cout << cudaGetErrorName(error) << endl;
          */
         DoIter<<<gridDim,blockDim>>>(d_Aold,d_Anew);
-        cudaDeviceSynchronize();
+        //cudaDeviceSynchronize();
         /*
         cout << "end " << i  << endl;
         error = cudaGetLastError();
@@ -196,6 +220,59 @@ int main(int argc, char** argv)
         */
     }
 
+    getVerificationValues<<<gridDim,blockDim>>>(d_Anew, d_check1, d_check2);
+    int N = rounded_n*rounded_n;
+    /*
+    if(cudaMemcpy(A_new,d_Anew,sizeof(double)*rounded_n*rounded_n,cudaMemcpyDeviceToHost) != cudaSuccess){
+        cout<<"Could not copy d_Anew into A_new"<<endl;
+        cudaError_t error = cudaGetLastError();
+        cout << cudaGetErrorString(error) << endl;
+        cout << cudaGetErrorName(error) << endl;
+    }
+    for(int i = 0; i < rounded_n; ++i){
+        for(int j=0;j<rounded_n;++j){
+            cout << A_new[i*rounded_n + j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl << endl;
+    */
+    while (N > 1) {
+        swap(d_Aold, d_Anew);
+        int rounded_N = N;
+        if (N % 1024 != 0) {
+            rounded_N = 1024 * (N / 1024 + 1);
+        }
+        /*
+        if (cudaMemcpy(d_n, &N, sizeof(int), cudaMemcpyHostToDevice) != cudaSuccess) {
+            cout << "Could not copy N into d_n" << endl;
+        }
+         */
+        reduce<<<rounded_N / 1024, 1024>>>(d_Aold, d_Anew, N);
+        cudaDeviceSynchronize();
+        N = rounded_N/1024;
+        /*
+        if(cudaMemcpy(A_new,d_Anew,sizeof(double)*rounded_n*rounded_n,cudaMemcpyDeviceToHost) != cudaSuccess){
+            cout<<"Could not copy d_Anew into A_new"<<endl;
+            cudaError_t error = cudaGetLastError();
+            cout << cudaGetErrorString(error) << endl;
+            cout << cudaGetErrorName(error) << endl;
+        }
+        for(int i = 0; i < N; ++i){
+            cout << A_new[i] << " ";
+        }
+        cout << endl << endl;
+        N = rounded_N/1024;
+         */
+    }
+
+    cudaDeviceSynchronize();
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     /*
     error = cudaGetLastError();
@@ -213,6 +290,7 @@ int main(int argc, char** argv)
     //because the CPU can continue executing after the kernel is launched. This behavior could be
     //prevented by using a call to cudaDeviceSynchronize()
     //note that destination is first in the cudaMemcpy call
+
     if(cudaMemcpy(A_new,d_Anew,sizeof(double)*rounded_n*rounded_n,cudaMemcpyDeviceToHost) != cudaSuccess){
         cout<<"Could not copy d_Anew into A_new"<<endl;
         cudaError_t error = cudaGetLastError();
@@ -220,14 +298,28 @@ int main(int argc, char** argv)
         cout << cudaGetErrorName(error) << endl;
     }
 
-    cout << std::accumulate(A_new, A_new + rounded_n*rounded_n, 0.0) <<  "  " << A_new[(n_/3)* rounded_n + (n_/3)]  << " "
-        << A_new[19*rounded_n + 37] << endl;
+    if(cudaMemcpy(&check1, d_check1, sizeof(double), cudaMemcpyDeviceToHost) != cudaSuccess){
+        cout<<"Could not copy d_check1 into check1"<<endl;
+    }
+    if(cudaMemcpy(&check2, d_check2, sizeof(double), cudaMemcpyDeviceToHost) != cudaSuccess){
+        cout<<"Could not copy d_check2 into check2"<<endl;
+    }
+    if(cudaMemcpy(&sumCheck, d_Anew, sizeof(double), cudaMemcpyDeviceToHost) != cudaSuccess){
+        cout<<"Could not copy d_Anew[0] into sumCheck"<<endl;
+    }
+    cout << std::setprecision(std::numeric_limits<double>::max_digits10 - 1) << "Sum: " << sumCheck <<  ",  A[n/3][n/3]:" << check1  << ", A[19][37]:" << check2 << ", Elapsed Time: " << elapsedTime << endl;
+     /*
+    sumCheck = std::accumulate(A_new, A_new + rounded_n*rounded_n, 0.0);
+    check1 = A_new[(n_/3)*rounded_n + (n_/3)];
+    check2 = A_new[19*rounded_n + 37];
 
+    cout << "Sum: " << sumCheck <<  ",  A[n/3][n/3]:" << check1  << ", A[19][37]:" << check2 << ", Elapsed Time: " << elapsedTime << endl;
+    */
     delete[] A;
     delete[] A_new;
     cudaFree(d_Anew);
     cudaFree(d_Aold);
-    cudaFree(d_sumCheck);
+    //cudaFree(d_n);
     cudaFree(d_check1);
     cudaFree(d_check2);
 
